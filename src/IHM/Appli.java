266a90646ca477774a46e99b3f62e19aaef6d22e
ClementSefrin <clementsefrin.pro@gmail.com>
2023-12-04 12:07:58 +0100

    a

--- src/IHM/Appli.java
+++ src/IHM/Appli.java
@@ -4,6 +4,7 @@ package IHM;
 public class Appli {
     //--------------------------------------Demander pour les conditions de fin de la partie--------------------------------------//
     public static void main(String[] args) {
+        System.out.println("test");
         Session.session();
     }
 }
